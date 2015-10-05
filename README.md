87c4a154702f13bd6e1b675c0b1842e0e455a193
alui <al.chevere@icloud.com>
2015-10-05 18:32:29 -0500

    Project moved to NetBeans. FXML initial views.
    Unit testing is ready. And also, JDBC.

--- README.md
+++
@@ -1,2 +0,0 @@
-# DMDB
-Desktop Movie Data Base. Uses SQLite to store the database, and it has a graphical user interface, to communicate.
---
+++ test/dmdb/ApplicationDelegateTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dmdb;
+
+import javafx.stage.Stage;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alfonso
+ */
+public class ApplicationDelegateTest {
+    
+    public ApplicationDelegateTest() {
+    }
+
+    /**
+     * Test of start method, of class ApplicationDelegate.
+     */
+    @Test
+    public void testStart() throws Exception {
+        System.out.println("start");
+        Stage stage = null;
+        ApplicationDelegate instance = new ApplicationDelegate();
+        instance.start(stage);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class ApplicationDelegate.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        ApplicationDelegate.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
